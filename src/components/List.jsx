a7fc008dc74c81453ad0108801ddce559550044e
Vibhor <ridervibhor1999@gmail.com>
2023-04-27 00:37:47 +0530

    Lecture-2

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>first</li>
+      <li>second</li>
+      <li>third</li>
+    </ul>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const date = new Date().getHours();
-
-var whatTimeOfDay = "";
-var customColor = "";
-
-if(date < 12) {
-  whatTimeOfDay =  "morning";
-  customColor = "red";
-} else if(date >= 12 && date <=18) {
-  whatTimeOfDay = "afternoon";
-  customColor = "green";
-} else {
-  whatTimeOfDay = "night";
-  customColor = "blue";
-}
-
-var customCss = {
-  color: customColor
-}
-
-console.log(date);
-
-ReactDOM.render(
-  <div style={customCss}>
-    <h1>Good {whatTimeOfDay}</h1>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
