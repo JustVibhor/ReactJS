c2059d5c415ef13ec3316a5ccafa54a1586e8b14
Vibhor <ridervibhor1999@gmail.com>
2023-04-27 15:31:11 +0530

    ES import export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import add, { subtract, multiply, divide } from "./calculator";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <ul>
+      <li>{add(1, 2)}</li>
+      <li>{subtract(1, 2)}</li>
+      <li>{divide(1, 2)}</li>
+      <li>{multiply(1, 2)}</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
